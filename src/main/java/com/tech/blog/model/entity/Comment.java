8e1ee21d0ae62850ff920586a6ee1805a9b1a8c8
manbarmohamed <johndoe@example.com>
2025-01-19 13:38:52 +0100

    Create Comment entity, CommentRepository, CommentService and CommentServiceImpl

---
+++ src/main/java/com/tech/blog/model/entity/Comment.java
@@ -0,0 +1,25 @@
+package com.tech.blog.model.entity;
+
+import jakarta.persistence.*;
+import java.time.LocalDateTime;
+
+@Entity
+public class Comment {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String content;
+    private LocalDateTime createAt;
+    private LocalDateTime updatedAt;
+
+    @ManyToOne
+    @JoinColumn(name = "user_id")
+    private User user;
+
+    @ManyToOne
+    @JoinColumn(name = "post_id")
+    private Post post;
+
+    // Getters, Setters, Constructors, etc.
+}
---
+++ src/main/java/com/tech/blog/service/impl/CommentServiceImpl.java
@@ -0,0 +1,6 @@
+package com.tech.blog.service.impl;
+
+import com.tech.blog.service.interfaces.CommentService;
+
+public class CommentServiceImpl implements CommentService {
+}
