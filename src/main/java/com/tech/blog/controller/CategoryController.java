9b03b07600be354bb859b73280eff22e3f373790
manbarmohamed <johndoe@example.com>
2025-01-20 23:57:15 +0100

    Make implementation of Save method in CategoryController

---
+++ src/main/java/com/tech/blog/controller/CategoryController.java
@@ -0,0 +1,37 @@
+package com.tech.blog.controller;
+
+
+import com.tech.blog.model.dto.request.CategoryCreateRequest;
+import com.tech.blog.model.dto.response.CategoryResponse;
+import com.tech.blog.service.interfaces.CategoryService;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.net.URI;
+
+@RestController
+@RequestMapping("/api/v1/categories")
+@RequiredArgsConstructor
+@Slf4j
+public class CategoryController {
+
+    private final CategoryService categoryService;
+
+
+    @PostMapping("/save")
+    @ResponseStatus(HttpStatus.CREATED)
+    public ResponseEntity<CategoryResponse> save
+            (@RequestBody @Valid
+             CategoryCreateRequest request) {
+        log.info("REST request to create Category");
+        CategoryResponse response = categoryService.save(request);
+        return ResponseEntity.created(URI.create("/api/v1/categories/save" + response.getId()))
+                .body(response);
+    }
+
+
+}
