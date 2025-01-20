f95e1270bd925a2afa10569d5187befb287e1985
manbarmohamed <johndoe@example.com>
2025-01-20 23:28:19 +0100

    Make implementation of save methode in the CategoryServiceImpl

--- src/main/java/com/tech/blog/service/impl/CategoryServiceImpl.java
+++ src/main/java/com/tech/blog/service/impl/CategoryServiceImpl.java
@@ -1,6 +1,58 @@
 package com.tech.blog.service.impl;
 
+import com.tech.blog.exception.ResourceNotFoundException;
+import com.tech.blog.mapper.CategoryMapper;
+import com.tech.blog.model.dto.request.CategoryCreateRequest;
+import com.tech.blog.model.dto.request.CategoryUpdateRequest;
+import com.tech.blog.model.dto.response.CategoryResponse;
+import com.tech.blog.model.entity.Category;
+import com.tech.blog.repository.CategoryRepository;
 import com.tech.blog.service.interfaces.CategoryService;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
 
+import java.util.List;
+
+
+@Service
+@RequiredArgsConstructor
+@Slf4j
 public class CategoryServiceImpl implements CategoryService {
+
+    private final CategoryRepository categoryRepository;
+    private final CategoryMapper categoryMapper;
+
+
+
+
+    @Override
+    public CategoryResponse save(CategoryCreateRequest request) {
+        log.info("Creating category Name: {}", request.getName());
+
+        Category category = categoryMapper.toEntity(request);
+        Category savedCategory = categoryRepository.save(category);
+        log.info("Category saved successfully with id: {}", savedCategory.getId());
+        return categoryMapper.toResponse(savedCategory);
+    }
+
+    @Override
+    public CategoryResponse update(Long id, CategoryUpdateRequest request) {
+        return null;
+    }
+
+    @Override
+    public void delete(Long id) {
+
+    }
+
+    @Override
+    public List<CategoryResponse> fetchAll() {
+        return List.of();
+    }
+
+    @Override
+    public CategoryResponse findById(Long id) {
+        return null;
+    }
 }
