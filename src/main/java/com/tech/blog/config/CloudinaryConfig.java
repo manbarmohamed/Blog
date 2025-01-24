e8b79e74ab4499d2b942f825325742419a9a01d9
manbarmohamed <manbar.mohammed@gmail.com>
2025-01-24 16:37:43 +0100

    Implement CloudinaryConfig

---
+++ src/main/java/com/tech/blog/config/CloudinaryConfig.java
@@ -0,0 +1,27 @@
+package com.tech.blog.config;
+
+import com.cloudinary.Cloudinary;
+import com.cloudinary.utils.ObjectUtils;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class CloudinaryConfig {
+
+    @Value("${CLOUD_NAME}")
+    String cloudName ;
+    @Value("${API_KEY}")
+    String apiKey ;
+    @Value("${API_SECRET}")
+    String apiSecret ;
+
+    @Bean
+    public Cloudinary cloudinary() {
+        return new Cloudinary(ObjectUtils.asMap(
+                "cloud_name", cloudName,
+                "api_key", apiKey,
+                "api_secret", apiSecret
+        ));
+    }
+}
